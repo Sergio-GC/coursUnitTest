fb3a362ce4f9552223729b6f1f3fe95a0a113e7f
Celinevialard <celine.vialard@gmail.com>
2023-09-23 10:32:52 +0200

    load and save not tested

--- Exercice 1/TP1/TP1_SergioCeline/Form1.Designer.cs
+++ Exercice 1/TP1/TP1_SergioCeline/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pbInit = new PictureBox();
+            cmbFilter = new ComboBox();
+            cmbAlgoEdge = new ComboBox();
+            btnLoadPicture = new Button();
+            btnTrasform = new Button();
+            btnSavePicture = new Button();
+            pbResult = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pbInit).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbResult).BeginInit();
+            SuspendLayout();
+            // 
+            // pbInit
+            // 
+            pbInit.Location = new Point(12, 12);
+            pbInit.Name = "pbInit";
+            pbInit.Size = new Size(420, 426);
+            pbInit.TabIndex = 0;
+            pbInit.TabStop = false;
+            // 
+            // cmbFilter
+            // 
+            cmbFilter.FormattingEnabled = true;
+            cmbFilter.Location = new Point(451, 82);
+            cmbFilter.Name = "cmbFilter";
+            cmbFilter.Size = new Size(185, 33);
+            cmbFilter.TabIndex = 1;
+            // 
+            // cmbAlgoEdge
+            // 
+            cmbAlgoEdge.FormattingEnabled = true;
+            cmbAlgoEdge.Location = new Point(451, 149);
+            cmbAlgoEdge.Name = "cmbAlgoEdge";
+            cmbAlgoEdge.Size = new Size(185, 33);
+            cmbAlgoEdge.TabIndex = 2;
+            // 
+            // btnLoadPicture
+            // 
+            btnLoadPicture.Location = new Point(451, 12);
+            btnLoadPicture.Name = "btnLoadPicture";
+            btnLoadPicture.Size = new Size(185, 48);
+            btnLoadPicture.TabIndex = 3;
+            btnLoadPicture.Text = "Load";
+            btnLoadPicture.UseVisualStyleBackColor = true;
+            btnLoadPicture.Click += btnLoadPicture_Click;
+            // 
+            // btnTrasform
+            // 
+            btnTrasform.Location = new Point(451, 219);
+            btnTrasform.Name = "btnTrasform";
+            btnTrasform.Size = new Size(185, 48);
+            btnTrasform.TabIndex = 4;
+            btnTrasform.Text = "Process";
+            btnTrasform.UseVisualStyleBackColor = true;
+            btnTrasform.Click += btnTrasform_Click;
+            // 
+            // btnSavePicture
+            // 
+            btnSavePicture.Location = new Point(451, 390);
+            btnSavePicture.Name = "btnSavePicture";
+            btnSavePicture.Size = new Size(185, 48);
+            btnSavePicture.TabIndex = 5;
+            btnSavePicture.Text = "Save";
+            btnSavePicture.UseVisualStyleBackColor = true;
+            btnSavePicture.Click += btnSavePicture_Click;
+            // 
+            // pbResult
+            // 
+            pbResult.Location = new Point(660, 12);
+            pbResult.Name = "pbResult";
+            pbResult.Size = new Size(420, 426);
+            pbResult.TabIndex = 6;
+            pbResult.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1115, 455);
+            Controls.Add(pbResult);
+            Controls.Add(btnSavePicture);
+            Controls.Add(btnTrasform);
+            Controls.Add(btnLoadPicture);
+            Controls.Add(cmbAlgoEdge);
+            Controls.Add(cmbFilter);
+            Controls.Add(pbInit);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pbInit).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbResult).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pbInit;
+        private ComboBox cmbFilter;
+        private ComboBox cmbAlgoEdge;
+        private Button btnLoadPicture;
+        private Button btnTrasform;
+        private Button btnSavePicture;
+        private PictureBox pbResult;
     }
 }
--- Exercice 1/TP1/TP1_SergioCeline/Form1.cs
+++ Exercice 1/TP1/TP1_SergioCeline/Form1.cs
@@ -1,3 +1,6 @@
+using System.Drawing;
+using System.Drawing.Imaging;
+
 namespace TP1_SergioCeline
 {
     public partial class Form1 : Form
@@ -6,5 +9,57 @@ namespace TP1_SergioCeline
         {
             InitializeComponent();
         }
-    }
+
+        private void btnLoadPicture_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFileDialog = new OpenFileDialog();
+            OpenFileDialog.Title = "Select image ";
+            OpenFileDialog.Filter = "Jpeg Images(*.jpg)|*.jpg|Png Images(*.png)|*.png|Bitmap Images(*.bmp)|*.bmp";
+
+            if (OpenFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                StreamReader streamReader = new StreamReader(OpenFileDialog.FileName);
+                Bitmap image = new Bitmap(streamReader.BaseStream);
+                streamReader.Close();
+
+                pbInit.Image = image;
+
+            }
+        }
+
+        private void btnTrasform_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSavePicture_Click(object sender, EventArgs e)
+        {
+           Bitmap image = (Bitmap) pbResult.Image;
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Specify a file name and file path";
+            sfd.Filter = "Png Images(*.png)|*.png|Jpeg Images(*.jpg)|*.jpg";
+            sfd.Filter += "|Bitmap Images(*.bmp)|*.bmp";
+
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string fileExtension = Path.GetExtension(sfd.FileName).ToUpper();
+                ImageFormat imgFormat = ImageFormat.Png;
+
+                if (fileExtension == "BMP")
+                {
+                    imgFormat = ImageFormat.Bmp;
+                }
+                else if (fileExtension == "JPG")
+                {
+                    imgFormat = ImageFormat.Jpeg;
+                }
+
+                StreamWriter streamWriter = new StreamWriter(sfd.FileName, false);
+                image.Save(streamWriter.BaseStream, imgFormat);
+                streamWriter.Flush();
+                streamWriter.Close();
+
+            }
+        }
 }
