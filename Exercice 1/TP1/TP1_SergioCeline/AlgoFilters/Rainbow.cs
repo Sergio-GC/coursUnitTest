7c60f3ed3ef3b103d18ed236a8085413897cf5b8
Sergio <gomez.c.sergio@outlook.com>
2023-09-26 20:30:42 +0200

    Refactoring

--- Exercice 1/TP1/TP1_SergioCeline/AlgoFilters/Rainbow.cs
+++ Exercice 1/TP1/TP1_SergioCeline/AlgoFilters/Rainbow.cs
@@ -12,34 +12,11 @@ namespace TP1_SergioCeline.AlgoFilters
         public override Bitmap algo(Bitmap image)
         {
             Bitmap result = new Bitmap(image.Width, image.Height);
-            //TODO pourquoi pas Width?
-            int raz = image.Height / 4;
+            int raz = image.Width / 5;
             for (int x = 0; x < image.Width; x++)
             {
-                //TODO on pourrait faire une fonction pour le choix de couleur ?
                 // Dividers for the colors
-                int r = 1, g = 1, b = 1;
-
-                // Current width zone for calculing the dividers
-                int currentZone = (int) (x / raz);
-                switch (currentZone)
-                {
-                    case 0:
-                        r = 5;
-                        break;
-                    case 1:
-                        g = 5;
-                        break;
-                    case 2:
-                        b = 5;
-                        break;
-                    case 3:
-                        r = b = 5;
-                        break;
-                    default:
-                        r = g = b = 5;
-                        break;
-                }
+                int []dividers = GetDividers(x, raz);
 
                 for (int y = 0; y < image.Height; y++)
                 {
@@ -49,10 +26,45 @@ namespace TP1_SergioCeline.AlgoFilters
                     byte blue = image.GetPixel(x, y).B;
 
                     // Change the colors
-                    result.SetPixel(x, y, Color.FromArgb(red / r, green / g, blue / b)) ;
+                    result.SetPixel(x, y, Color.FromArgb(red / dividers[0], green / dividers[1], blue / dividers[2])) ;
                 }
 
             }
+            return result;
+        }
+
+        /// <summary>
+        /// Calculates the values to divide the rgb colors of an image in order to apply a rainbow filter.
+        /// </summary>
+        /// <param name="horizontalPixel">Position of the current pixel horizontally (by the width)</param>
+        /// <param name="sideWidth">Width of each side to determine in which size is the pixel</param>
+        /// <returns>Array with integer dividers in rgb order</returns>
+        private int[] GetDividers(int horizontalPixel, float sideWidth)
+        {
+            // By default, dividers are equal to 1
+            int[] result = {1, 1, 1};
+            int currentArea = (int)(horizontalPixel / sideWidth);
+
+            // Calculate the dividers in function of the current position
+            switch(currentArea)
+            {
+                case 0:
+                    result[0] = 5;
+                    break;
+                case 1:
+                    result[1] = 5;
+                    break;
+                case 2:
+                    result[2] = 5;
+                    break;
+                case 3:
+                    result[0] = result[2] = 5;
+                    break;
+                default:
+                    result[0] = result[1] = result[2] = 5;
+                    break;
+            }
+
             return result;
         }
     }
