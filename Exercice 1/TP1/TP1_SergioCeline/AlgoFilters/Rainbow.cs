7a314025909ed466ed1241370a5dd11e3eef8b04
Sergio <gomez.c.sergio@outlook.com>
2023-11-04 08:33:04 +0100

    Remove unused imports

--- Exercice 1/TP1/TP1_SergioCeline/AlgoFilters/Rainbow.cs
+++ Exercice 1/TP1/TP1_SergioCeline/AlgoFilters/Rainbow.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TP1_SergioCeline.AlgoFilters
+﻿namespace TP1_SergioCeline.AlgoFilters
 {
     public class Rainbow : AlgoFilter
     {
