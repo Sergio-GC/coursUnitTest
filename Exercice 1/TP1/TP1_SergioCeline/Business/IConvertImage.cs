ddca26a38001bdeb94a78dabf058677925b110aa
Céline Vialard <celine.vialard@gmail.com>
2023-11-09 20:46:57 +0100

    save and load in db?

--- Exercice 1/TP1/TP1_SergioCeline/Business/IConvertImage.cs
+++ Exercice 1/TP1/TP1_SergioCeline/Business/IConvertImage.cs
@@ -1,7 +1,11 @@
-﻿namespace TP1_SergioCeline.Business
+﻿using System.Drawing.Imaging;
+
+namespace TP1_SergioCeline.Business
 {
     public interface IConvertImage
     {
         public Bitmap ConvertToBitmap(Image image);
+        public byte[] ConvertImageToByteArray(Image imageToConvert,
+                                      ImageFormat formatOfImage);
     }
 }
--- Exercice 1/TP1/TP1_SergioCeline/Tools/ConvertImage.cs
+++ Exercice 1/TP1/TP1_SergioCeline/Tools/ConvertImage.cs
@@ -1,4 +1,5 @@
-﻿using TP1_SergioCeline.Business;
+﻿using System.Drawing.Imaging;
+using TP1_SergioCeline.Business;
 
 namespace TP1_SergioCeline.Tools
 {
@@ -7,6 +8,21 @@ namespace TP1_SergioCeline.Tools
         public Bitmap ConvertToBitmap(Image image)
         {
             return new Bitmap(image);
+        }
+        public byte[] ConvertImageToByteArray(Image imageToConvert,
+                                      ImageFormat formatOfImage)
+        {
+            byte[] Ret;
+            try
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    imageToConvert.Save(ms, formatOfImage);
+                    Ret = ms.ToArray();
+                }
+            }
+            catch (Exception) { throw; }
+            return Ret;
         }
     }
 }
